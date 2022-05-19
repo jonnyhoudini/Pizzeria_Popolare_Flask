8e638cff2e0f2bcafac35ac6a7fb5072c9c40f94
jonnyhoudini <jmcaveety@gmail.com>
2022-05-19 21:17:57 +0100

    finished extensions

--- controllers/controller.py
+++ controllers/controller.py
@@ -2,10 +2,11 @@ from flask import render_template
 from app import app
 from models.order_list import pizzas
 
-@app.route('/')
+@app.route('/orders')
 def index():
-    return render_template('index.html', pizzas=pizzas)
+    return render_template('index.html', title="Order Management at Pizzeria Popolare", pizzas=pizzas)
 
 @app.route('/orders/<index>')
 def get_order(index):
-    return return render_template('order.html', pizzas=pizzas)
+    chosen_order = pizzas[int(index)]
+    return render_template('order.html', order=chosen_order)
