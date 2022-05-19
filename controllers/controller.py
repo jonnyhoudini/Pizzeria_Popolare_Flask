eb7e80b34a1e69e35f4b9e58080683b51edb1d72
jonnyhoudini <jmcaveety@gmail.com>
2022-05-19 10:33:19 +0100

    final changes before homework submission

--- controllers/controller.py
+++ controllers/controller.py
@@ -4,12 +4,8 @@ from models.order_list import pizzas
 
 @app.route('/')
 def index():
-    return "Pizzeria Popolare"
-
-@app.route('/<name>')
-def greet(name):
-    return f"Benvenuto to the Pizzeria Popolare, {name}!"
-
-@app.route('/orders')
-def get_orders():
     return render_template('index.html', pizzas=pizzas)
+
+@app.route('/orders/<index>')
+def get_order(index):
+    return return render_template('order.html', pizzas=pizzas)
